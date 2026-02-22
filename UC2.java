bda3ab9b67ccd9683cce88695fab234e963eb5fe
sinha-aayush <a.sinha282006@gmail.com>
2026-02-23 00:31:44 +0530

    UC1

--- UC2.java
+++ UC2.java
@@ -1,4 +1,4 @@
-public class UC1 {
+public class UC2 {
     public static void main(String[] args) {
         System.out.println("  ***      ***      ******      *****  ");
         System.out.println(" ** **    ** **     **  **     **   ** ");
