30ab71a3e3cac116ed45dfaa11a85d7e61bfe8f7
sinha-aayush <a.sinha282006@gmail.com>
2026-02-23 23:00:18 +0530

    :wq

--- UC3.java
+++ UC3.java
@@ -2,13 +2,13 @@ public class UC3 {
     public static void main(String[] args) {
         // Constructing each line by joining the individual letter segments
         System.out.println(String.join("   ","  ***  ", "   ***  ", "****** ", " ***** "));
-        System.out.println(String.join("  ", " **  ** " ," **  ** ","**    **","**      "));
-        System.out.println(String.join("  ", "**    **", "**    **","**    **"," **      "));
-        System.out.println(String.join("  ", "**    **", "**    **","****** ", "   ***   "));
-        System.out.println(String.join("  ", "**    **", "**    **","**     ", "     ** "));
-        System.out.println(String.join("  ", "**    **", "**    **","**     ", "      ** "));
-        System.out.println(String.join("  ", " **   **", " **  ** ","**     ", "     ** "));
-        System.out.println(String.join("   ", "  ***  ", "  ***  ", "**     ", "*****   "));
+        System.out.println(String.join("  "," **  ** " ," **  ** ","**    **","**      "));
+        System.out.println(String.join("  ","**    **", "**    **","**    **"," **      "));
+        System.out.println(String.join("  ","**    **", "**    **","****** ","   ***   "));
+        System.out.println(String.join("  ","**    **", "**    **","**     ","     ** "));
+        System.out.println(String.join("  ","**    **", "**    **","**     ","      ** "));
+        System.out.println(String.join("  "," **   **", " **  ** ","**     ","     ** "));
+        System.out.println(String.join("   ","  ***  ","  ***  ","**     "," *****   "));
     }
 }
  
