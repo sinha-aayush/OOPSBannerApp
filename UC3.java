40f2beac7dcd92ca964c690a785607e1bcd34c62
sinha-aayush <a.sinha282006@gmail.com>
2026-02-23 23:22:39 +0530

    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ .git/COMMIT_EDITMSG [unix] (22:59 23/02/2026)                                                                    7,1 All "~/Desktop/java_prog/.git/COMMIT_EDITMSG" [unix] 12L, 308B ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ .git/COMMIT_EDITMSG [unix] (22:59 23/02/2026)                                                                    7,1 All "~/Desktop/java_prog/.git/COMMIT_EDITMSG" [unix] 12L, 308B

---
+++ UC3.java
@@ -0,0 +1,14 @@
+public class UC3 {
+    public static void main(String[] args) {
+        // Constructing each line by joining the individual letter segments
+        System.out.println(String.join("   ","  ***  ", "   ***  ", "****** ", " ***** "));
+        System.out.println(String.join("  ", " **  ** " ," **  ** ","**    **","**      "));
+        System.out.println(String.join("  ", "**    **", "**    **","**    **"," **      "));
+        System.out.println(String.join("  ", "**    **", "**    **","****** ", "   ***   "));
+        System.out.println(String.join("  ", "**    **", "**    **","**     ", "     ** "));
+        System.out.println(String.join("  ", "**    **", "**    **","**     ", "      ** "));
+        System.out.println(String.join("  ", " **   **", " **  ** ","**     ", "     ** "));
+        System.out.println(String.join("   ", "  ***  ", "  ***  ", "**     ", "*****   "));
+    }
+}
+ 
